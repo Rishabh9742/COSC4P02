bbc206774c6927086a99b577227f728851c1e5ef
Jashan Pannu <jp18jj@brocku.ca>
2023-03-09 15:35:56 -0500

    Skeleton timeline class

---
+++ timeline.py
@@ -0,0 +1,55 @@
+from Node import Node
+from linked_list import LinkedList
+
+class timeline:
+    def __init__(self) -> None:
+        self.linkedList = None
+        self.currentNode = None
+        return
+    
+    def loadTimeLine(self) -> None:
+        return
+    
+    def saveTimeLine(self) -> None:
+        return
+    
+    def startTimeline(self) -> None:
+        return
+    
+    def nextEvent(self) -> None:
+        return
+    
+    def previousEvent(self) -> None:
+        return
+    
+    def getTitle(self) -> str:
+        return
+    
+    def getSummary(self) -> str:
+        return
+    
+    def getDate(self) -> str:
+        return
+    
+    def getFacts(self) -> str:
+        return
+    
+    def setTitle(self) -> None:
+        return
+    
+    def setSummary(self) -> None:
+        return
+    
+    def setDate(self) -> None:
+        return
+    
+    def setFacts(self) -> None:
+        return
+    
+    def addEvent(self) -> None:
+        return
+    
+    def deleteEvent(self) -> None:
+        return
+
+    
