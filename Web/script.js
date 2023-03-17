f15afe8916e0a8f2a64b9eee3d406f7af873cefa
Alex <32850744+RABlue27@users.noreply.github.com>
2023-03-17 16:56:30 -0400

    Basic web component.
    Same concept as tkinter. Needs data hosted online to work for CORS reasons. Linking to this github repo for data.csv works fine,

---
+++ Web/script.js
@@ -0,0 +1,69 @@
+let lineNum = 0;
+let data;
+
+async function showData(line_num) {
+    const response = await fetch('https://raw.githubusercontent.com/Rishabh9742/COSC4P02/main/data.csv');
+    const csvData = await response.text();
+    const rows = csvData.split('\n').map(row => row.split(','));
+    data = rows.slice(0, -1);
+
+  
+    try {
+      const [title, description, image_url] = data[line_num];
+  
+      document.getElementById('label_title').textContent = title;
+      document.getElementById('label_description').textContent = description;
+  
+      const image = await loadImage(image_url);
+      document.getElementById('label_image').src = image.src;
+  
+      const line_text = `Line ${line_num + 1} of ${data.length}`;
+      document.getElementById('label_line').textContent = line_text;
+    } catch (error) {
+      console.error(error);
+    }
+  
+    // console.log(data[lineNum]);
+  }
+  
+  showData(lineNum);
+
+
+  function loadImage(url) {
+    return new Promise((resolve, reject) => {
+      const image = new Image();
+      image.onload = () => resolve(image);
+      image.onerror = () => reject(new Error(`Failed to load image from ${url}`));
+      image.src = url;
+    });
+  }
+
+function nextLine() {
+  if (lineNum < data.length - 1) {
+    lineNum++;
+    showData(lineNum);
+  }
+}
+
+function previousLine() {
+  if (lineNum > 0) {
+    lineNum--;
+    showData(lineNum);
+  }
+}
+
+function jumpToLine() {
+  const input = document.getElementById('jumpToLine');
+  const newLineNum = parseInt(input.value) - 1;
+  if (!isNaN(newLineNum)) {
+    lineNum = newLineNum;
+    showData(lineNum);
+    input.value = '';
+  }
+}
+
+document.getElementById('previous').addEventListener('click', previousLine);
+document.getElementById('goToLine').addEventListener('click', jumpToLine);
+document.getElementById('next').addEventListener('click', nextLine);
+
+showData(lineNum);
