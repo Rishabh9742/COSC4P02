d02317fe12d04d3939d9658ec33112c5e9c2ed98
Jashan Pannu <jp18jj@brocku.ca>
2023-04-28 15:21:50 -0400

    Bugfix

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -132,11 +132,12 @@ class LinkedList:
        
     def rate_node(node_name, rating):
         node_data[node_name]["ratings"].append(rating)
+        return
 
-        # Function to allow users to submit a review for a node
+    # Function to allow users to submit a review for a node
     def submit_review(node_name, review):
         node_data[node_name]["reviews"].append(review)
-
-         # Example usage
-         rate_node("item1", 4)
-         submit_review("item1", "Good item!")
+        #Example usage
+        rate_node("item1", 4)
+        submit_review("item1", "Good item!")
+        return
